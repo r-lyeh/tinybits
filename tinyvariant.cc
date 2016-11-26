bff9d856d89f8e155f33e7593e7c14e2ee23c624
r-lyeh <r-lyeh@users.noreply.github.com>
2016-11-26 17:05:17 +0100

    Update tinyvariant.cc

--- tinyvariant.cc
+++ tinyvariant.cc
@@ -83,11 +83,10 @@ template<>           inline const double& cast<double>(const var &v) { return v.
 template<>           inline const std::string& cast<std::string>(const var &v) { return *v.string; }
 template<>           inline const std::function<void()>& cast<std::function<void()>>(const var &v) { return *v.callback; }
 
-
-
+#ifdef TINYVARIANT_MAIN
 #include <iostream>
 #include <assert.h>
-int main() {
+int TINYVARIANT_MAIN() {
     std::cout << "sizeof(var)=" << sizeof(var) << std::endl;
 
     var digit = 42, other = "hello world";
@@ -102,3 +101,4 @@ int main() {
     std::cout << other << std::endl;
     other();
 }
+#endif
