8b04372f411b9933d64efc64d93801c0f4f0d9eb
r-lyeh <35402248+r-lyeh@users.noreply.github.com>
2018-04-19 11:47:52 +0200

    Update tinyfsm.c

--- tinyfsm.c
+++ tinyfsm.c
@@ -1,8 +1,11 @@
 // Tiny FSM. rlyeh, public domain | wtrmrkrlyeh
 
-#   define fsm(st)     for(int i=1;i--;st[1]=st[0]) switch(((st[0])<<16)|(st[1]))
-#   define when(a)     break; case (((a)<<16)|(a))
-#   define during(a,b) break; case (((b)<<16)|(a))
+#pragma once
+#define with(st)        for(int i=1;i--;st[1]=st[0]) switch(((st[0])<<16)|(st[1]))
+#define when(a)         break; case (((a)<<16)|(a))
+#define transition(a,b) break; case (((b)<<16)|(a))
+typedef int fsm[2];
+
 
 #ifdef TINYFSM_DEMO
 #include <stdio.h>
@@ -13,24 +16,24 @@ enum {
     RUNNING,
 };
 
-void update( int *state ) {
-    fsm(state) {
-        when(IDLE):              puts("idle");
-        during(IDLE,WALKING):    puts("idle --> walking");
-        during(IDLE,RUNNING):    puts("idle --> running");
+void update( fsm state ) {
+    with(state) {
+        when(IDLE):                  puts("idle");
+        transition(IDLE,WALKING):    puts("idle --> walking");
+        transition(IDLE,RUNNING):    puts("idle --> running");
 
-        when(WALKING):           puts("walking");
-        during(WALKING,IDLE):    puts("walking --> idle");
-        during(WALKING,RUNNING): puts("walking --> running");
+        when(WALKING):               puts("walking");
+        transition(WALKING,IDLE):    puts("walking --> idle");
+        transition(WALKING,RUNNING): puts("walking --> running");
 
-        when(RUNNING):           puts("running");
-        during(RUNNING,IDLE):    puts("running --> idle");
-        during(RUNNING,WALKING): puts("running --> walking");
+        when(RUNNING):               puts("running");
+        transition(RUNNING,IDLE):    puts("running --> idle");
+        transition(RUNNING,WALKING): puts("running --> walking");
     }
 }
 
 void TINYFSM_DEMO() {
-    int state[2] = {0};
+    fsm state = {0};
 
     *state = IDLE;
     update(state);
