532892b73cd6b9c3e50f1df3ea59bc03f5544bc7
r-lyeh <35402248+r-lyeh@users.noreply.github.com>
2020-02-13 21:22:06 +0100

    Update tinyerror.c

--- tinyerror.c
+++ tinyerror.c
@@ -1,42 +1,43 @@
-// simple error handling api: error(N, "optional description"), errorcode, errortext
+// simple error handling api. non-intrusive version.
 // - rlyeh, public domain.
+// 
+// Errors automatically printed in debug builds.
+// Usage: use return OK(retvalue) or return ERROR(retvalue, "error"...); as desired.
+// Usage: to check for errors: if(ERROR) { /* do something here */ }
+// Good habit: system errorcode first, then human explanation when reporting errors. Ie, ERROR(rc, "404 File not found %s\n", file);
 
-#define error(rc, ...)   (errortext = !(rc) ? "No error 0":"Error -- " error_stringize(rc) ": " __VA_ARGS__)
-#define errorcode        (atoi(errortext+9))
-
-// utils
-
-#define error_stringize(rc)  error_stringiz3(rc)
-#define error_stringiz3(rc)  #rc
-
-#if defined _MSC_VER && !defined(__thread)
-#define __thread __declspec(thread)
-#endif
-
-static __thread char* errortext = "No error 0";
-
-
-/*
+#pragma once
 #include <stdio.h>
 
-FILE *open_file(const char *file) {
-    error(0);
-    FILE *fp = fopen(file, "rb");
-    if( !fp ) error(404, "Cannot open file");
-    return fp;
-}
+#  if !defined ERROR_LOG && defined NDEBUG
+#define ERROR_LOG(...)       (void)0
+#elif !defined ERROR_LOG
+#define ERROR_LOG(...)       (fprintf(stderr, __VA_ARGS__), fprintf(stderr, "\n"))
+#endif
 
-int main() {
-    FILE *fp = open_file(__FILE__); //"missing.txt");
+#define OK(retval)         (ERROR = (char*)0, (retval))
+#define ERROR(retval, ...) (ERROR_LOG(""__VA_ARGS__), ERROR = (char*)"Error: " #__VA_ARGS__ " (@" __FUNCTION__ " " __FILE__ ":" ERR0R(__LINE__) ") ", (retval))
+#define ERR0R(rc)           ERRoR(rc)
+#define ERRoR(rc)           #rc
 
-    if( !errorcode ) {
-        printf("ok\n");
-    }
-    if( errorcode ) {
-        printf("%d\n", errorcode);
-    }
-    if( errortext ) {
-        printf("%s\n", errortext);
-    }
+#  if  defined _MSC_VER && !defined(__thread)
+#define __thread __declspec(thread)
+#elif !defined _MSC_VER && !defined __FUNCTION__
+#define __FUNCTION__ ""
+#endif
+static __thread char* ERROR = 0;
+
+#if 0 // demo
+int derefence(int *ptr) {
+    if(ptr) return OK(*ptr);
+    return ERROR(0, "404: Cannot deference pointer [%p]", ptr); // errorcode + variable message (ideal)
+    return ERROR(0, "Cannot deference pointer [%p]", ptr);      // variable message (no errorcode)
+    return ERROR(0, "Cannot deference pointer");                // fixed message (no errorcode)
+    return ERROR(0);                                            // (no message) (no errorcode)
 }
-*/
+int main(int arg, char **argv) {
+    int a = derefence(&arg); // pass
+    int b = derefence(NULL); // fail
+    if( ERROR ) { /* do something here */ }
+}
+#endif
