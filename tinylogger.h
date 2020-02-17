c055671dab97e6d4f2d6cc996a4cc19346728c74
r-lyeh <35402248+r-lyeh@users.noreply.github.com>
2020-02-17 21:48:49 +0100

    Update tinylogger.h

--- tinylogger.h
+++ tinylogger.h
@@ -33,28 +33,27 @@
 
 #define LOG(...) do { \
     static char must_log = -1; \
-    static enum { undefined, base, r, g, y, p, t, } color = undefined; \
-    const int colors[] = { 0,0,31,32,33,35,36, }; /*also, blue:34,white:37*/ \
+    static enum { undefined=-1, restore=0, r=31,g,y,b,p,t,w } color = undefined; \
     char buf[2048]; snprintf(buf, 2048, "" __VA_ARGS__); \
     if( color == undefined ) { \
+        LOG_ENABLE_ANSI(); \
         char low[2048]; int i; for(i=0;buf[i];++i) low[i] = 32|buf[i]; low[i]='\0'; \
         /**/ if( strstr(low,"fatal")|| strstr(low,"panic") || strstr(low,"assert") ) color=r,must_log=1; \
         else if( strstr(low,"fail") || strstr(low,"error") ) color=r; \
         else if( strstr(low,"warn") || strstr(low,"alert") ) color=y; /*beware,caution*/ \
         else if( strstr(low,"info") || strstr(low,"succe") ) color=g; /*ok, no error*/ \
         else if( strstr(low,"debug") ) color=t; \
-        else if( strstr(low,"trace") ) color=p; \
+        else if( strstr(low,"trace") ) color=p; else color=restore; \
         if( must_log < 0 ) { /* original splitmix64 by Sebastiano Vigna (CC0)*/ \
             uint64_t z = (__LINE__ + __COUNTER__ + UINT64_C(0x9E3779B97F4A7C15)); \
             z = (z ^ (z >> 30)) * UINT64_C(0xBF58476D1CE4E5B9); \
             must_log = (unsigned)((z ^ (z >> 31)) % 100) < LOG_LEVEL; } } \
     if( must_log ) { \
-        LOG_ENABLE_ANSI(); \
         double timer = LOG_TIMER(); \
-        if(timer>0)fprintf(stderr, "\033[%dm%07.3fs %s (%s:%d)\n", colors[color], timer, buf, __FILE__, __LINE__); \
-        else       fprintf(stderr, "\033[%dm%s (%s:%d)\n", colors[color], buf, __FILE__, __LINE__); \
+        if(timer>0)fprintf(stderr, "\033[%dm%07.3fs %s (%s:%d)\n", color, timer, buf, __FILE__, __LINE__); \
+        else       fprintf(stderr, "\033[%dm%s (%s:%d)\n", color, buf, __FILE__, __LINE__); \
         if(color == r) LOG_PRINT_STACK(); \
-        fprintf(stderr, "\033[%dm", colors[base]); /*restore color*/ } \
+        fprintf(stderr, "\033[%dm", restore); } \
 } while(0)
 
 #if 0 // demo
